479989426d3bde5c86e419b56e5c3ac24a7299c1
Renascence <geniusglory@outlook.com>
2017-05-12 20:36:35 +0800

    add webpack hot load

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 // import express from 'express';
 var express = require('express')
+var webpack = require('webpack');
+var config = require('../webpack.config');
+var path = require('path')
+
 const app = express();
 
+app.use(express.static('./build'));
+
+var options ={
+    publicPath: "/",
+}
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile('./src/router/index.html');
 });
 
 var server = app.listen(3000, function () {
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,44 @@
+var webpack = require('webpack');
 var path = require('path');
+var fs = require('fs');
+var nodeModules = {};
 
 module.exports = {
-  entry: './src/app.js',
+  context: resolve(__dirname, 'src'),
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+
+    path.resolve(__dirname, './src/app/main.js'),
+
+  ],
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.js',
+    path: path.resolve(__dirname, './build'),
+    publicPath: '/',
+    filename: 'main.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    // 开启服务器的模块热替换(HMR)
+
+    contentBase: path.resolve(__dirname, 'build'),
+    // 输出文件的路径
+    publicPath: '/'
+    // 和上文 output 的“publicPath”值保持一致
+  },
+
   module: {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
+      loader: 'babel-loader',
+    }],
   },
-  node: {
-    fs: "empty",
-    net: 'empty'
-  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+
+  ]
 };
