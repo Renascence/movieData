320aef9788e50efa7a7853fc0fe93639bd20ef90
logging <geniusglory@outlook.com>
2017-10-10 00:18:48 +0800

    add eslint

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-// import express from 'express';
-var express = require('express')
-var webpack = require('webpack');
-var config = require('../webpack.config');
-var path = require('path')
+// // import express from 'express';
+// var express = require('express')
+// var webpack = require('webpack');
+// var config = require('../webpack.config');
+// var path = require('path')
 
-const app = express();
+// const app = express();
 
-app.use(express.static('./build'));
+// app.use(express.static('./build'));
 
-var options ={
-    publicPath: "/",
-}
+// var options ={
+//     publicPath: "/",
+// }
 
 
-app.get('/', function (req, res) {
-  res.sendfile('./src/router/index.html');
-});
+// app.get('/', function (req, res) {
+//   res.sendfile('./src/router/index.html');
+// });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+// var server = app.listen(3000, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+//   console.log('Example app listening at http://%s:%s', host, port);
+// });
---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <span>hello world</span>;
+}
+
+export default App;
