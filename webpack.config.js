b6a8a34b01fd999d1bf0b4db289f4e4828fe887c
logging <geniusglory@outlook.com>
2017-10-28 23:39:59 +0800

    add less/css loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: __dirname + "/src/main.js",
+  entry: __dirname + "/src/App.js",
   output: {
     path: __dirname + "/build",
     filename: 'main.js',
@@ -23,11 +23,29 @@ module.exports = {
   },
 
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-    }],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          plugins: [
+            ['import', [{ libraryName: "antd", style: true }]],
+          ]
+        }
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }, {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "less-loader"
+        }]
+      }]
   },
   plugins: [
     new HtmlWebpackPlugin({
