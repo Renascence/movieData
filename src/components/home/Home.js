20236ad4a478d3f328b0263650f203b4d891cae8
logging <geniusglory@outlook.com>
2017-10-29 23:55:17 +0800

    feat: show rate in moviecard & add link for card

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Row, Col } from 'antd';
 import MovieCard from './MovieCard';
 
 
 function App(props) {
-  return (<div>
-    <Row type='flex' gutter={8}>
-      {
-        props.movie.map(item => <Col md={4} sm={6} xs={8} key={item.id} ><MovieCard {...item} /></Col>)
-      }
-    </Row>
-  </div>);
+  return (
+    <div>
+      <Row type="flex" gutter={8}>
+        {
+          props.movie.map(item =>
+            <Col md={4} sm={6} xs={8} key={item.id} ><MovieCard {...item} /></Col>)
+        }
+      </Row>
+    </div>
+  );
 }
 
 App.propTypes = {
-  movie: React.PropTypes.array,
+  movie: PropTypes.arrayOf(React.PropTypes.array).isRequired,
 };
 
 function mapStateToProps({ movie }) {
--- src/components/home/MovieCard.js
+++ src/components/home/MovieCard.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router-dom';
+import { Rate } from 'antd';
 import './home.less';
 
 function MovieCard(props) {
-  const { images, title } = props;
-  return <div className='movieCard'>
-      <img src={images.medium} />
-    <p>{title}</p>
-  </div>
+  const { images, title, rating } = props;
+  return (
+    <div className="movieCard">
+      <Link to="/">
+        <img src={images.medium} alt={title} />
+        <p>{title}</p>
+        <Rate allowHalf value={rating.average / 2} />
+      </Link>
+    </div>
+  );
 }
+
+MovieCard.propTypes = {
+  images: PropTypes.objectOf(React.PropTypes.object).isRequired,
+  rating: PropTypes.objectOf(React.PropTypes.object).isRequired,
+  title: PropTypes.string.isRequired,
+};
 
 export default MovieCard;
